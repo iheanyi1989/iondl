d10ec3247a2f73343895c0d2daf60b8da1a11de3
Iheanyi Onwubiko <ioncloudjourney@gmail.com>
2022-06-21 09:52:27 -0400

    migrating state to TF cloud

---
+++ backend.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  cloud {
+    organization = "iheanyi1989"
+    workspaces {
+      name = "iondl"
+    }
+  }
+
+provider "aws" {
+    region = "us-east-1"
+}
+}
